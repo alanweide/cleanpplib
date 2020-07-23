16444734147ed28280937a7a55a682479a696ab0
Alan Weide <alan.d.weide@gmail.com>
2020-07-23 15:58:37 -0400

    Beginning attempt to use templates to indicate polymorphism; new gitignore

---
+++ include/cleanpp/template_nn/template_nn.hpp
@@ -0,0 +1,26 @@
+//
+//  template_nn.hpp
+//  Cleanpp
+//
+//  Created by Alan Weide on 7/23/20.
+//  Copyright © 2020 Alan Weide. All rights reserved.
+//
+
+#ifndef template_nn_hpp
+#define template_nn_hpp
+
+#include <stdio.h>
+#include <clean_base.hpp>
+
+namespace cleanpp {
+
+template <class I>
+class template_nn_kernel: public clean_base {
+private:
+	std::unique_ptr<template_nn_kernel> val_;
+public:
+	
+}
+}
+
+#endif /* template_nn_hpp */
---
+++ src/template_nn/template_nn.cpp
@@ -0,0 +1,9 @@
+//
+//  template_nn.cpp
+//  Cleanpp
+//
+//  Created by Alan Weide on 7/23/20.
+//  Copyright © 2020 Alan Weide. All rights reserved.
+//
+
+#include "template_nn.hpp"
