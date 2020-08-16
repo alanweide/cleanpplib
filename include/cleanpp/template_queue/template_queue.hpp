62ffdf4ead6b7f0f3661ff9e3a3eb63dbb9d1adc
Alan Weide <alan.d.weide@gmail.com>
2020-08-16 15:53:24 -0400

    Fixed a small formatting issue

--- include/cleanpp/template_queue/template_queue.hpp
+++ include/cleanpp/template_queue/template_queue.hpp
@@ -20,80 +20,80 @@ namespace cleanpp {
 
 template <template<typename> class I, typename Item>
 class t_queue: public clean_base {
-    /*
-     queue is modeled by string of Item
-     */
+	/*
+	 queue is modeled by string of Item
+	 */
 private:
-    std::unique_ptr<queue<Item>> rep_;
+	std::unique_ptr<queue<Item>> rep_;
 public:
-    
+	
 	/*
 	 ensures this = <>
 	 */
-    t_queue() {
-        rep_ = std::make_unique<I<Item>>();
-    }
-    
-    t_queue(const t_queue<I, Item> &o) = delete;
+	t_queue() {
+		rep_ = std::make_unique<I<Item>>();
+	}
+	
+	t_queue(const t_queue<I, Item> &o) = delete;
 	/*
 	 clears  other
 	 ensures this = #other
 	 */
-    t_queue(t_queue<I, Item>&& other): rep_(std::move(other.rep_)) {
-        other.rep_ = std::make_unique<I<Item>>();
-    }
-    
-    t_queue<I, Item>& operator=(const t_queue<I, Item>& other) = delete;
+	t_queue(t_queue<I, Item>&& other): rep_(std::move(other.rep_)) {
+		other.rep_ = std::make_unique<I<Item>>();
+	}
+	
+	t_queue<I, Item>& operator=(const t_queue<I, Item>& other) = delete;
 	/*
 	 clears  other
 	 ensures this = #other
 	 */
-    t_queue<I, Item>& operator=(t_queue<I, Item>&& other) {
+	t_queue<I, Item>& operator=(t_queue<I, Item>&& other) {
 		if (&other == this) {
 			return *this;
 		}
 		rep_ = std::move(other.rep_);
 		other.rep_ = std::make_unique<I<Item>>();
 		return *this;
-    }
-    
+	}
+	
 	/*
 	 clears this
 	 */
-    void clear() {
+	void clear() {
 		this->rep_->clear();
-    }
-
-    /*
-     updates this
-     clears  x
-     ensures this = #this * <x>
-     */
-    void enqueue(Item&& x) {
-        rep_->enqueue(std::forward<Item>(x));
-    }
-    
-    /*
-     updates  this
-     requires |this| > 0
-     ensures  this * <dequeue> = #this
-     */
+	}
+	
+	/*
+	 updates this
+	 clears  x
+	 ensures this = #this * <x>
+	 */
+	void enqueue(Item&& x) {
+		rep_->enqueue(std::forward<Item>(x));
+	}
+	
+	/*
+	 updates  this
+	 requires |this| > 0
+	 ensures  this * <dequeue> = #this
+	 */
 	Item dequeue() {
-        return rep_->dequeue();
-    }
-    
-    /*
-     ensures is_empty = (|this| = 0)
-     */
+		return rep_->dequeue();
+	}
+	
+	/*
+	 ensures is_empty = (|this| = 0)
+	 */
 	bool is_empty() const {
-        return rep_->is_empty();
-    }
-    
-    friend std::ostream& operator<<(std::ostream& out, t_queue<I, Item>& o) {
-        return out << *o.rep_;
-    }
+		return rep_->is_empty();
+	}
+	
+	friend std::ostream& operator<<(std::ostream& out, t_queue<I, Item>& o) {
+		return out << *o.rep_;
+	}
 };
 
-        }
-        
+}
+
 #endif /* template_queue_h */
