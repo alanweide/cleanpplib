7c83eedfd440d401e0c0127672b4e3d981d0e278
alanweide <alan.d.weide@gmail.com>
2020-11-17 15:30:03 -0500

    updated minimal example to show it (mostly) working on mac

--- cleanpp-main/minimal.cpp
+++ cleanpp-main/minimal.cpp
@@ -1,10 +1,13 @@
 #include <memory>
+#include <iostream>
 
-std::unique_ptr<int>&& foo() {
-    std::unique_ptr<int> x = std::make_unique<int>(0);
+std::unique_ptr<int>&& foo(std::unique_ptr<int>&& y) {
+    std::unique_ptr<int> x = std::forward<std::unique_ptr<int>>(y);
     return std::move(x);
 }
 
 int main() {
-    std::unique_ptr<int> y = foo();
+    std::unique_ptr<int> y = std::make_unique<int>(5);
+    y = foo(std::move(y));
+    std::cout << "y = " << *y << "\n";
 }
